5d3ad9f4e83744ad8295967a17432260b08399bd
Elizabeth Alexander <eealexander2@gmail.com>
2016-07-16 15:01:46 -0500

    Add input form

--- fakeFacebook/app/controllers/sessions_controller.rb
+++ fakeFacebook/app/controllers/sessions_controller.rb
@@ -1,24 +1,23 @@
 class SessionsController < ApplicatonControlller
 
-#here, we must inherit from the application controller which is predefined for us 
- 
+#here, we must inherit from the application controller which is predefined for us
 
-	def create 
+
+	def create
 		@user = User.find_by(email: params[:email])
 		if @user && @user.authenticate(params[:password])
-			session[:user_id] = @user.id 
-			redirect_to posts_path #this will give you all of the posts 
-		else 
+			session[:user_id] = @user.id
+			redirect_to posts_path #this will give you all of the posts
+		else
 			flash[:danger] = "Invalid Login"
 			render "new"
-		end 
-	end 
+		end
+	end
 
-	def destroy 
-		session.clear # you would want to clear the session here 
-		current_user = nil  #you would then set the current user back equal to nil here so that we know what is happening
-		redirect_to login_path
+	def destroy
+		session.clear
+		current_user = nil
 
-	end 
+	end
 
-end 
+end
--- fakeFacebook/app/controllers/users_controller.rb
+++ fakeFacebook/app/controllers/users_controller.rb
@@ -1,68 +1,66 @@
 class UsersController < ApplicationController
 
-	#remember it all so you can do it again 
-	#we don't want a user index page because we will not want to show all of the users 
+	#remember it all so you can do it again
+	#we don't want a user index page because we will not want to show all of the users
 	def new
-		@user = User.new 
+		@user = User.new
 		#i could do this all day, like literally all day
 		#get request to get to page to create new user
-	end 
+	end
 
 	def create
 		@user = User.new(user_params)
-		if @user.save 
-			session[:user_id] = @user.id 
-			redirect_to posts_path 
-		else 
+		if @user.save
+			session[:user_id] = @user.id
+			redirect_to posts_path
+		else
 			flash[:danger] = "Invalid Registration. Please try again!"
 			render "new"
-			# we do not need to set the current_user yet 
-		end 
-	end 
+			# we do not need to set the current_user yet
+		end
+	end
 
-	def show 
+	def show
 		@user = User.find(session[:user_id])
 		@posts = []
 		Post.all.each do |post|
-			if post.receiver_id == @user.id || post.poster_id == @user.id 	
-				@posts << post 
+			if post.receiver_id == @user.id || post.poster_id == @user.id
+				@posts << post
 			end
-		end  
-		p @posts 
-		# i don't think you need to render anything here. You are good to go 
-		# in my opinion
-	end 
+		end
+		p @posts
+	end
 
-	def edit 
+	def edit
 		@user = User.find(params[:id]) #BC this would be the users' path that we need to see.
 		if @user != current_user
 			flash[:danger] = "Oops! You are not allowed to update the profile of other people!"
-			redirect_to edit_user_path #I think this is right ? 
+			redirect_to edit_user_path #I think this is right ?
 		end
 		#because you always know where the session ID will be, I gues
-	end 
+	end
 
-	def update 
+	def update
 		@user = User.find(params[:id])
-		if @user != current_user 
+		if @user != current_user
 			flash[:danger] = "You do not have the proper access to edit this page."
-			redirect_to edit_user_path 
-		end 
-		if  
-		 	@user.update(edit_user_params) #these are hard params that you define later 
+			redirect_to edit_user_path
+		end
+		if
+		 	@user.update(edit_user_params) #these are hard params that you define later
 		 	redirect_to @user
 		else
 			render "edit"
-		end  
+		end
 	end
 
-	private 
+	private
 
-	def user_params 
+	def user_params
 		params.require(:user).permit(:name, :email, :address, :photo, :password_digest)
-	end  
+	end
 
-	def edit_user_params 
+	def edit_user_params
 		params.require(:user).permit(:name, :email, :photo)
-	end 
-end 
+	end
+end
